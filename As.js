af058eb60a6c66a5f0fb5f7451639c72da8a2e5a
cynthiawambuamumbua <cynthiawambuamumbua@gmail.com>
2023-03-14 13:23:12 +0300

    first commit

---
+++ As.js
@@ -0,0 +1,35 @@
+let x="extravaganza"
+console.log(x[8],x[9],x[10],x[11])
+
+let b = "eat"
+const food ="The quick fox jumped over the lazy dog"
+const food1 = `The ${b} quick fox jumped over the lazy dog`
+console.log(food1)
+
+
+var a="the"
+var c="brown"
+const story="The quick fox jumped over the lazy dog"
+const string=`The quick fox jumped over the lazy dog`
+console.log(string.count,["the","brown"]) //[].length;
+
+
+var y="are"
+const string1="The pupils are reading in the library";
+console.log(string1.search("are"))
+var t="sitting"
+const string2="The child was sitting on the table before it fell"
+console.log(string2.search("sitting"))
+
+var q="wonderful"
+console.log(q.toUpperCase());
+var g="amazing"
+console.log(g.toLowerCase())
+var z="UndERneath"
+console.log(z.toLowerCase()) 
+var c="A wonderful world"
+const f=`A wonderful world`
+console.log(f.toLocaleUpperCase())
+
+
+
